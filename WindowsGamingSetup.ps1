00999067e20ce762e64be8bb072cafd3265f8a98
jollyroger53 <70240868+jollyroger53@users.noreply.github.com>
2020-09-06 20:29:00 +0100

    Update WindowsGamingSetup.ps1

--- WindowsGamingSetup.ps1
+++ WindowsGamingSetup.ps1
@@ -1,12 +1,10 @@
 ## WORK IN PROGRESS ##
 
-
-### This script is intended to be run on fresh installs of Windows 10 to apply specific settings, registry tweaks, power settings, install programs, etc. This script can also be used as a template and tailored for your own liking. 
-### This script has been tailored to my personal preferences, if you want to tweak it feel free to fork or download the script and modify it yourself. For example, I've chosen 'Brave' as my browser of choice, but if you'd prefer Firefox for example, replace the "$browser" variable with the download URL for Firefox.
+## How to use ##
+# run the following line in an elevated command prompt:
+# powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/jollyroger53/thepirateship/master/WindowsGamingSetup.ps1')"
 
 ### Templates ###
-
-# If you're happy with this script as it is, you can run it with a single line using the following line in an elevated command prompt: powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/jollyroger53/thepirateship/master/WindowsGamingSetup.ps1')"
 
 # This is an example of a program install line: 
 #   $browser = *www.url-of-browser.com/install.exe* 
@@ -15,7 +13,6 @@
 #   Invoke-WebRequest "$browser" -Outfile $directory\$browserinstaller
 #   Start-Process -FilePath $directory\$browserinstaller
 
-
 ### Variables ###
 
 # Programs
@@ -23,7 +20,7 @@
 $directory = 
 $browser = https://github.com/brave/brave-browser/releases/download/v0.59.33/BraveBrowserSilentSetup.exe
 $browserinstaller = BraveBrowserSilentSetup.exe
-$notepad++ = https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.8.9/npp.7.8.9.Installer.x64.exe
+$notepadpp = https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.8.9/npp.7.8.9.Installer.x64.exe
 $notepadinstaller = npp.7.8.9.Installer.x64.exe
 $steam = https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe
 $msiafterburner = http://download.msi.com/uti_exe/vga/MSIAfterburnerSetup.zip
@@ -34,4 +31,16 @@ $7zip = https://www.7-zip.org/a/7z1900-x64.exe
 
 # Power setting template (included in my custom .iso)
 
-$powerconfig = 
+$powerconfig = C:\maxpower.pow
+
+# Other
+
+
+# Pre-run checker to check that the user is using the correct iso file. 
+
+if(Test-Path C:\maxpower.pow) {
+    Write-Host 'The script is now starting'
+}else {
+    Write-Host 'Incorrect .iso file used, script is now exiting.'
+exit
+}
